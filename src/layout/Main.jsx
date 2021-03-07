b1606a6a58463aa11bd17656b094d536a34d24a9
z707 <z707@example.com>
2021-03-07 09:06:23 +0300

    fetch and show data

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,35 @@
-function Main() {
-    return (
-        <main className="container content">
-            Hello
-        </main>
-    )
+import React from 'react';
+import {Movies} from '../components/Movies';
+
+class Main extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            movies: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=70662215&s=matrix')
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}));
+    }
+
+    render() {
+        const {movies} = this.state;
+
+        return (
+            <main className="container content">
+                {
+                    movies.length
+                    ? (<Movies movies={movies}/>)
+                    : <h5>Loading...</h5>
+                }
+            </main>
+        )
+    }
 }
 
 export {Main}
